90e99bd1cad836e5ff6ab398a143f452655f5a1c
yk4716 <yk4716@bs.amalnet.k12.il>
2025-01-04 13:49:45 +0200

    getting started

--- app/src/main/java/com/example/ex0602_basics/MainActivity.java
+++ app/src/main/java/com/example/ex0602_basics/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ex0602_basics;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +10,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn= (button) findViewById(R.id.button);
+        button.setOnClickListener();
     }
 }
+    public void go(View view) {
+
+    }
